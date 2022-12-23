69b7d649c2954f6b78402bf2189afa964b15a6e3
Stevexeu <steftijssen@gmail.com>
2022-12-23 16:58:42 +0100

    Character Controller Update

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -5,8 +5,39 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [Header("Enemy Stats")]
+    [SerializeField] private float _health = 9;
+    [SerializeField] private bool isAlive = true;
+    Animator animator;
+
+    public float Health
+    {
+        set
+        {
+            if (value < _health)
+            {
+                animator.SetTrigger("takeDamage");
+            }
+
+            _health = value;
+            if(_health <= 0)
+            {
+                animator.SetBool("isAlive", false);
+            }
+        }
+        get
+        {
+            return _health;
+        }
+    }
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void OnHit(float damage)
     {
-        Debug.Log("Damage received: " + "[" + damage + "]");
+        Health -= damage;
     }
 }
